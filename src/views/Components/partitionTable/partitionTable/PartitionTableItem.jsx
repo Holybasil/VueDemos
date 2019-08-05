95f8aaf68154990a75f48df92b7e92e29f15789a
Holybasil <youyulan@huli.com>
2019-08-05 13:59:28 +0800

    update partitionTable

--- src/views/Components/partitionTable/partitionTable/PartitionTableItem.jsx
+++ src/views/Components/partitionTable/partitionTable/PartitionTableItem.jsx
@@ -1,70 +1,69 @@
-import './PartitionTableItem.scss'
-const componentName = 'PartitionTableItem'
+import "./PartitionTableItem.scss";
+const componentName = "PartitionTableItem";
 export default {
   name: componentName,
+  data() {
+    return {
+      // i: 0
+    };
+  },
   props: {
     label: {
       type: String
     }
   },
+  created() {
+    console.log(this.partitionTable.spanColumn, "table");
+  },
   methods: {
-    renderEmptyValueItem (label) {
+    renderEmptyValueItem(label) {
       return (
         <div class="emptyValueItem">
           <span>{label}</span>
-          <span></span>
+          <span />
         </div>
-      )
+      );
     },
-    renderEmptyItem () {
-      return (
-        <div class="emptyItem"></div>
-      )
+    renderEmptyItem() {
+      return <div class="emptyItem" />;
     },
-    renderTextItem (label, value) {
+    renderTextItem(label, value) {
       return (
         <div class="textItem">
           <span>{label}</span>
           <span>{value}</span>
         </div>
-      )
+      );
     },
-    renderItem (label, vnodeList) {
+    renderItem(label, vnodeList) {
       return (
         <div class="item">
           <span>{label}</span>
-          {
-            vnodeList
-          }
-
+          {vnodeList}
         </div>
-      )
+      );
     },
-    mapSlotsDefaultToView () {
+    mapSlotsDefaultToView() {
       if (!this.$slots.default) {
         if (this.label) {
           // debugger
-          return this.renderEmptyValueItem(this.label)
+          return this.renderEmptyValueItem(this.label);
         }
-        return this.renderEmptyItem()
+        return this.renderEmptyItem();
       } else {
-        const [...vnodeList] = this.$slots.default
+        const [...vnodeList] = this.$slots.default;
         // 属于纯文本
         if (vnodeList.length === 1 && vnodeList[0].text) {
-          return this.renderTextItem(this.label, vnodeList[0].text)
+          return this.renderTextItem(this.label, vnodeList[0].text);
         }
-        return this.renderItem(this.label, vnodeList)
+        return this.renderItem(this.label, vnodeList);
       }
     }
   },
-  render (h) {
+  inject: ["partitionTable"],
+  render() {
+    // const { i } = this
     // console.log(this.$slots.default, this.label, '$slots.default')
-    return (
-      <div class="partitionTableItem">
-        {
-          this.mapSlotsDefaultToView()
-        }
-      </div>
-    )
+    return <div class="partitionTableItem">{this.mapSlotsDefaultToView()}</div>;
   }
-}
+};
--- src/views/Components/partitionTable/partitionTable/index.jsx
+++ src/views/Components/partitionTable/partitionTable/index.jsx
@@ -1,5 +1,11 @@
 
 export default {
+  data () {
+    return {
+      deep: 1,
+      spanColumn: 0
+    }
+  },
   props: {
     // 表格的数据
     data: {
@@ -11,10 +17,10 @@ export default {
       default: false
     },
     // 斑马纹的深度
-    deep: {
-      type: Number,
-      default: 1
-    },
+    // deep: {
+    //   type: Number,
+    //   default: 1
+    // },
     // 值为几列
     column: {
       type: Number,
@@ -30,15 +36,73 @@ export default {
       }
     }
   },
+  created () {
+    this.getGridSpanFromData(this.$slots.default)
+    // if(this.column)
+    // deep 2 column 2
+    this.spanColumn = 2 * this.column + this.deep - 1
+    console.log(this.deep, 'deep')
+  },
+  methods: {
+    haveComponent (children) {
+      return children.filter(child => {
+        return child.tag
+      }).length
+    },
+    getGridSpanFromData (data) {
+      // debugger
+      let nextData = []
+      if (!data.length) return
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].componentOptions && data[i].componentOptions.children && this.haveComponent(data[i].componentOptions.children)) {
+          nextData = [...nextData, ...data[i].componentOptions.children]
+        }
+      }
+      if (nextData.length) {
+        this.deep++
+        nextData.forEach((child) => {
+          child.deep = this.deep
+        })
+      }
+      this.getGridSpanFromData(nextData)
+    },
+    getDataItemDeep (item) {
+      let deep = 0
+      if (item.children) {
+        deep++
+        // this.getDataItemDeep()
+        item.children.forEach((child) => {
+          this.getDataItemDeep(child)
+        })
+      }
+      return deep
+    }
+    // getPartitionTableItem(){
+
+    // }
+  },
+  provide () {
+    return {
+      partitionTable: this
+    }
+  },
   render () {
     console.log(this.$slots.default, 'partitionTable')
+    // const { data } = this
     return (
 
-      <div className="PartitionTable">
+      <div class="PartitionTable">
         {
           this.$slots.default
         }
       </div>
+      // <div className="PartitionTable">
+      //   {
+      //     data.map((item, index) => {
+      //       this.getPartitionTableItem(item)
+      //     })
+      //   }
+      // </div>
     )
   }
 }
